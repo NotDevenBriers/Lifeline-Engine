d033ad1f48420c81bfc5e29a4e2b3b711fdc6c2c
NotDevenBriers <devenb02@gmail.com>
2026-05-21 14:43:28 +0100

    -Implemented opengl libs -Included glad -Included GLFW

--- LifelineEngine/LifelineEngine/main.cpp
+++ LifelineEngine/LifelineEngine/main.cpp
@@ -1,10 +1,32 @@
-#include "Renderer.h"
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 
-int main() {
+#include <iostream>
 
-	while (true) {
-		Renderer::renderFrame();
-	}
+int main()
+{
+    glfwInit();
 
-	return 0;
+    GLFWwindow* window =
+        glfwCreateWindow(1280, 720, "Engine", nullptr, nullptr);
+
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGL())
+    {
+        std::cout << "Failed to initialize GLAD\n";
+        return -1;
+    }
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+
+    return 0;
 }
